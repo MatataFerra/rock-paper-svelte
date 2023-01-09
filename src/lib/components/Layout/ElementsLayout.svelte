6f75aa2dc1fb47ee4a58a5644a08399c29f201f2
Matias Ferraro <mferraro@steplix.com>
2023-01-09 15:43:09 +0100

    fix responsive design

--- src/lib/components/Layout/ElementsLayout.svelte
+++ src/lib/components/Layout/ElementsLayout.svelte
@@ -1,10 +1,9 @@
 <script>
-	import { fade } from 'svelte/transition';
 	export let customStyles = '';
 </script>
 
 <section
-	class="relative h-full p-4 grid grid-cols-2 items-center content-center justify-items-center {customStyles}"
+	class=" lg:w-2/4 2xl:w-1/4 lg:mx-auto lg:my-0 relative h-full p-4 grid grid-cols-2 items-center content-center justify-items-center {customStyles}"
 >
 	<slot />
 </section>
